4c9f62562c655ae15a65ec429e992f63789f3ef5
thetkc312 <thetkc312@gmail.com>
2024-09-24 17:21:50 -0600

    3DScan module setup
    Adding basic attributes and methods of the 3DScan module, without implementation of the technical functions (removing outliers, kmeans, etc.)

--- README.md
+++ README.md
@@ -48,3 +48,38 @@ Desired GUI Features:
         - .parquet
         - .yaml
         - .json
+
+
+Class panel
+    attributes
+        name
+        normal
+        centriod
+        Hpoefully some how well get the orientation of a 3D axis coordinate system
+    methods
+        getters
+        setters
+        what else??
+
+class entire array
+    attributes
+        list of panels
+        amounts of panels in the list
+        what else??
+    methods
+        comparing panels (this will evolve into lots of methods)
+        add panel to the list
+        create a panel from json
+        create panels from group (group is from scan class)
+        create json
+        read json
+
+class scan 
+    attributes
+        Point cloud data
+    mehtods
+        create groups
+        give the groups to entire array
+        emliminate outliars
+
+
--- source/utilities.py
+++ source/utilities.py
@@ -14,3 +14,32 @@ def get_angle_between_two_vectors(vector_one, vector_two) -> float:
 def get_magnitude_of_vector(vector: tuple) -> float:
     magnitude = np.sqrt(np.square(vector[0]) + np.square(vector[1]) + np.square(vector[2]))
     return magnitude
+
+
+# TODO: Implement this function
+# This function is used by the 3DScan module various times, in various places, on both the point list as a whole and then on the individually grouped points, and this is is her as a util
+def find_outliers(points: np.array, method: str | None = 'trev_iter') -> np.array:
+    # This will take a 2d numpy array of points and use either ransac or Trevor's iterative method to generate a 1d map indicating which rows (representing points) in this collection are to be kept (False for outliers)
+    if method == 'trev_iter':
+        outlier_exclusion_map = remove_outliers_trev_iter(points)
+    elif method == 'ransac':
+        outlier_exclusion_map = remove_outliers_ransac(points)
+    elif method is None:
+        outlier_exclusion_map = np.full(points.shape[0], True)
+    else:
+        raise NotImplementedError
+    
+    return outlier_exclusion_map
+
+# TODO: Implement this function
+def remove_outliers_trev_iter(points: np.array) -> np.array:
+    pass
+
+# TODO: Implement this function
+def remove_outliers_ransac(points: np.array) -> np.array:
+    pass
+    
+
+
+# Trevor suggested that we make a conversion function that takes any 
+# form of an array and converts it to numpy or something that we choose
