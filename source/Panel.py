971dc27d1298bfb3df2b069f2b5efa6bebef4b35
Jake Sutton <jas546@byu.edu>
2024-09-28 16:29:24 -0600

    Made it so the panel class will return attributes. It also accepts tuples and np.arrays in the constructor

--- source/Panel.py
+++ source/Panel.py
@@ -1,14 +1,20 @@
-from typing import Any
+from typing import Any, Tuple, Union
+import numpy as np
 
 
 class Panel:
-    def __init__(self, vector: tuple, centroid: tuple, name: str) -> None:
-        self.normal_unit_vector = vector
-        self.panel_centroid = centroid
+    def __init__(self, vector: Union[np.array, Tuple[float, float, float]], 
+                 centroid: Union[np.array,Tuple[float, float, float]], 
+                 name: str) -> None:
+        self.normal_unit_vector = np.array(vector)
+        self.panel_centroid = np.array(centroid)
         self.panel_name = name
 
     def __getattribute__(self, name: str) -> Any:
-        pass
+        return super().__getattribute__(name)
 
-    def get_normal_vector(self) -> tuple:
-        return self.normal_unit_vector
+    def __getattribute__(self, vector: np.array) -> Any:
+        return super().__getattribute__(vector)
+    
+    def __getattribute__(self, centroid: np.array) -> Any:
+        return super().__getattribute__(centroid)
---
+++ tests/test_Panel.py
@@ -0,0 +1,23 @@
+from source.Panel import *
+
+def test_intialize_a_panel1():
+    panel_name = "Test Panel 1"
+    test_normal_vector = (5.5, -6.5, 2.0)
+    test_centroid = (0.0, 0.0, 0.0)
+
+    test_panel = Panel(test_normal_vector, test_centroid, panel_name)
+
+    assert test_panel.panel_name == panel_name # assert will only pass if the function returns true
+    assert np.array_equal(test_panel.normal_unit_vector, test_normal_vector) # assert will only pass if the function returns true
+    assert np.array_equal(test_panel.panel_centroid, test_centroid) # assert will only pass if the function returns true
+
+def test_initialize_a_panel2():
+    panel_name = "Test Panel 1"
+    test_normal_vector = np.array([5.5, -6.5, 2.0])  # Initialize as np.array
+    test_centroid = np.array([0.0, 0.0, 0.0])        # Initialize as np.array
+
+    test_panel = Panel(test_normal_vector, test_centroid, panel_name)
+
+    assert test_panel.panel_name == panel_name  # Assert will only pass if the function returns true
+    assert np.array_equal(test_panel.normal_unit_vector, test_normal_vector)  # Assert will only pass if the function returns true
+    assert np.array_equal(test_panel.panel_centroid, test_centroid)  # Assert will only pass if the function returns true
