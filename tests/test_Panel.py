68281a857aa71a0f822c7cadcc8980647386a19b
Jake Sutton <jas546@byu.edu>
2024-09-28 16:30:00 -0600

    wrote tests for the new Panel class

--- tests/test_Panel.py
+++ tests/test_Panel.py
@@ -2,8 +2,8 @@ from source.Panel import *
 
 def test_intialize_a_panel1():
     panel_name = "Test Panel 1"
-    test_normal_vector = (5.5, -6.5, 2.0)
-    test_centroid = (0.0, 0.0, 0.0)
+    test_normal_vector = (5.5, -6.5, 2.0) # Initialize as tuple
+    test_centroid = (0.0, 0.0, 0.0)       # Initialize as tuple
 
     test_panel = Panel(test_normal_vector, test_centroid, panel_name)
 
@@ -12,7 +12,7 @@ def test_intialize_a_panel1():
     assert np.array_equal(test_panel.panel_centroid, test_centroid) # assert will only pass if the function returns true
 
 def test_initialize_a_panel2():
-    panel_name = "Test Panel 1"
+    panel_name = "Test Panel 2"
     test_normal_vector = np.array([5.5, -6.5, 2.0])  # Initialize as np.array
     test_centroid = np.array([0.0, 0.0, 0.0])        # Initialize as np.array
 
