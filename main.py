95a0ca26fb7a244637024da8478bd5927ac76225
Jake Sutton <jas546@byu.edu>
2024-10-11 18:57:46 -0600

    started a sandbox in main, ill finish it later

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from source.Panel import Panel
 from source.Array import Array
+from source.Scan import Scan
 from tests.test_utilities import *
 from source.Visualizer import *
 import numpy as np
@@ -8,3 +9,12 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 print('hello world')
+
+class TestScan(Scan):
+    def __init__(self, data_array: np.ndarray, amount_of_clusters: int) -> None:
+        super().__init__(scan_filepath=None, amount_of_clusters=amount_of_clusters)  # Call the parent constructor
+        self.array_of_3D_points = data_array
+
+sandbox_scan = TestScan(create_random_dataset())
+
+sandbox_scan.create_clusters()
